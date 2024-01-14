589378e26cfdda6770b5686ebe5fc0baeb7dec50
Kelina Marina <marina311@mail.ru>
2024-01-14 13:16:31 +0500

    Create codeforces.js

---
+++ codeforces.js
@@ -0,0 +1,28 @@
+try {
+  document.querySelector('.standings tbody tr th:nth-of-type(2)').style.width='200px';
+} catch (error) {
+  console.error(error);
+}
+
+const dict = {
+"KVL": "Лихачев Константин"
+
+};
+
+// Rename users
+document.querySelectorAll('.rated-user').forEach(a => {
+  const key = a.innerHTML.trim();
+  if (key in dict) {
+    a.innerHTML = dict[key];
+  }
+});
+
+// Hide flag
+document.querySelectorAll('.standings-flag').forEach(a => {
+  a.remove();
+});
+
+// Hide to-practice
+const aElements = document.querySelectorAll('.change-participant-type-link').forEach(a => {
+  a.remove()
+});
